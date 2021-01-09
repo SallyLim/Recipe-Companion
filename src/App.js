2d6066621962a5279e2bcc1bf275e4ddeb967988
SallyLim <lim.sally20@gmail.com>
2021-01-09 15:36:51 -0800

    mock landing page done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './burger.svg';
 import './App.css';
+import URLForm from './Component/URLForm.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <URLForm />
       </header>
     </div>
   );
---
+++ src/Component/URLForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class URLForm extends React.Component {
+    render() {
+        return (
+            <form>
+                <h1> Please enter the URL of the recipe below. </h1>
+                <input
+                type="text"
+              />
+            </form>
+        );
+    }
+}
