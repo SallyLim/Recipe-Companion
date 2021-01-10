da3aef3fd806ab14a9bdc3a772aacf14b82b02e3
VincentSastra <vincent.sastra@gmail.com>
2021-01-10 00:50:36 -0800

    Put the list of all substitutes

---
+++ src/backend/substituteList.js
@@ -0,0 +1,204 @@
+var megaList = [
+  {
+    original: "Allspice",
+    subs: [
+      ["½ teaspoon cinnamon", "½ teaspoon ground cloves"]
+    ]
+  },
+  {
+    original: "Apple Pie Spice",
+    subs: [
+      ["½ teaspoon cinnamon", "¼ teaspoon nutmeg", "1/8 teaspoon cardamom"]
+    ]
+  },
+  {
+    original: "Baking Powder, double-acting",
+    subs: [
+      ["¼ teaspoon baking soda", "5/8 teaspoon cream of tartar"]
+    ]
+  },
+  {
+    original: "Barbeque Sauce",
+    subs: [
+      ["¾ cup ketchup", "2 tablespoons mustard", "2 tablespoons brown sugar"]
+    ]
+  },
+  {
+    original: "Butter",
+    subs: [
+      ["1 cup margarine (regular)"],
+      ["1 cup vegetable shortening (for baking)"],
+      ["1 cup of oil (for melted butter"]
+    ]
+  },
+  {
+    original: "Buttermilk",
+    subs: [
+      ["1 tablespoon lemon juice", "1 cup warm regular milk"],
+      ["1 tablespoon vinegar", "1 cup warm regular milk"]
+    ]
+  },
+  {
+    original: "Chili Sauce",
+    subs: [
+      ["1 cup tomato sauce", "¼ cup brown sugar", "2 tablespoons vinegar", "¼ teaspoon cinnamon", "1 dash of ground cloves", "1 dash of allspice"]
+    ]
+  },
+  {
+    original: "Chocolate, unsweetened",
+    subs: [
+      ["3 tablespoons cocoa", "1 tablespoon butter"],
+      ["3 tablespoons cocoa", "1 tablespoon regular margarine"],
+      ["3 tablespoons cocoa", "1 tablespoon vegetable oil"]
+    ]
+  },
+  {
+    original: "Cornstarch (for thickening)",
+    subs: [
+      ["2 tablespoons flour"]
+    ]
+  },
+  {
+    original: "Cream, whipping",
+    subs: [
+      [ "2 cups whipped cream"]
+    ]
+  },
+  {
+    original: "Egg",
+    subs: [
+      ["¼ cup egg substitute; check label for specific directions"],
+      ["Reconstituted powdered eggs; follow package directions"],
+      ["2 tablespoons mayonnaise (suitable for use in cake batter)"],
+      ["½ teaspoon baking powder", "1 tablespoon vinegar", "1 tablespoon liquid (for baking use only)"]
+    ]
+  },
+  {
+    original: "Flour, all-purpose white flour",
+    subs: [
+      ["½ cup whole wheat flour", "½ cup all-purpose flour"]
+    ]
+  },
+  {
+    original: "Flour, cake",
+    subs: [
+      ["14/16 intablespoons all-purpose flour"]
+    ]
+  },
+  {
+    original: "Flour, self-rising",
+    subs: [
+      ["46/48 teaspoons all-purpose flour", "1 ½ teaspoons baking powder", "½ teaspoon salt"]
+    ]
+  },
+  {
+    original: "Garlic",
+    subs: [
+      ["1/8 teaspoon garlic powder"]
+    ]
+  },
+  {
+    original: "Herbs, fresh",
+    subs: [
+      ["1 teaspoon dried leaf herbs", "½ teaspoon ground dried herbs"]
+    ]
+  },
+  {
+    original: "Ketchup (for use in cooking)",
+    subs: [
+      ["1 cup tomato sauce", "½ cup sugar", "2 tablespoons vinegar"]
+    ]
+  },
+  {
+    original: "Lemon Zest (fresh grated lemon peel)",
+    subs: [
+      ["½ teaspoon lemon extract"]
+    ]
+  },
+  {
+    original: "Marshmallows, miniature",
+    subs: [
+      ["10 large marshmallows"]
+    ]
+  },
+  {
+    original: "Mayonnaise (for use in salads and salad dressings)",
+    subs: [
+      ["1 cup sour cream"],
+      ["1 cup yogurt"],
+      ["1 cup cottage cheese pureed in a blender"]
+    ]
+  },
+  {
+    original: "Mustard, dry (in cooked mixtures)",
+    subs: [
+      ["1 tablespoon prepared mustard"]
+    ]
+  },
+  {
+    original: "Onion",
+    subs: [
+      ["1 tablespoon instant minced onion; check label for specific directions"]
+    ]
+  },
+  {
+    original: "Pumpkin Pie Spice",
+    subs: [
+      ["½ teaspoon cinnamon", "¼ ground teaspoon ginger", "1/8 teaspoon ground allspice", "1/8 teaspoon ground nutmeg"]
+    ]
+  },
+  {
+    original: "Rice",
+    subs: [
+      ["Most rice products will substitute for each other on a fairly equal basis in recipes but the cooking time and amount of liquid will vary. Learn more about cooking rice."]
+    ]
+  },
+  {
+    original: "Rum",
+    subs: [
+      ["1 part rum extract", "3 parts water"]
+    ]
+  },
+  {
+    original: "Sugar, Confectioners or powdered",
+    subs: [
+      ["1 cup granulated sugar", "1 tablespoon cornstarch; process in a food processor using the metal blade attachment until it's well blended and powdery."]
+      ]
+  },
+  {
+    original: "Tomato Juice",
+    subs: [
+      ["3/2 cup tomato sauce", "½ cup water"]
+    ]
+  },
+  {
+    original: "Tomato Soup",
+    subs: [
+      ["1 cup tomato sauce", "¼ cup water"]
+    ]
+  },
+  {
+    original: "Wine, red",
+    subs: [
+      ["1 cup grape juice"],
+      ["1 cup cranberry juice"]
+    ]
+  },
+  {
+    original: "Wine, white",
+    subs: [
+      ["1 cup apple juice"],
+      ["1 cup white grape juice"]
+    ]
+  },
+  {
+    original: "Yeast, compressed",
+    subs: [
+      ["¼ ounce active dry yeast", "9/4 teaspoons active dry yeast"]
+    ]
+  }
+]
+
+export function getMegaList() {
+  return megaList;
+}
