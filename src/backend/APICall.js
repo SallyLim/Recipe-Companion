64479c5cafbfb885449f1a15b067e78cac21afe2
VincentSastra <vincent.sastra@gmail.com>
2021-01-10 02:59:41 -0800

    API call fully working

--- src/backend/APICall.js
+++ src/backend/APICall.js
@@ -1,5 +1,14 @@
 export async function getIngredients(url) {
-  return fetch("/ingredient:" + url)
+  return fetch("/ingredient:" + url.replaceAll('/', "%2F"))
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res)
+      return res;
+    })
+}
+
+export async function getInstruction(url) {
+  return fetch("/instruction:" + url.replaceAll('/', "%2F"))
     .then((res) => res.json())
     .then((res) => {
       console.log(res)
