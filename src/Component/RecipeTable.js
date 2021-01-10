12ebe3e77641cb85978cfc57e05c7602d7a536b1
SallyLim <lim.sally20@gmail.com>
2021-01-09 21:36:23 -0800

    started working on adding the tablular feature for ingredients

--- src/Component/RecipeTable.js
+++ src/Component/RecipeTable.js
@@ -2,9 +2,15 @@ import React from "react";
 
 class RecipeTable extends React.Component {
     render() {
+        const headings = [
+            'Have in Pantry',
+            'Ingredients',
+            'Substitutes'
+        ]
+
         return (
             <div className="recipe-table">
-                <h1> hi </h1>
+                <h3> Title of the food </h3>
             </div>
         );
     }
